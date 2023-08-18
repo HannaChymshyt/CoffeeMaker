504238ae5c0621c262d6a15cd5933da42eb26668
Anna <anna.chymshyt@gmail.com>
2023-08-18 17:18:18 +0300

    Add Main

---
+++ src/coffee_maker/Main.java
@@ -0,0 +1,14 @@
+package coffee_maker;
+
+
+public class Main {
+    static CoffeeMachine coffeeMachine;
+    static int userChoice;
+
+    public static void main(String[] args) {
+
+        coffeeMachine = new CoffeeMachine();
+
+    }
+
+}
